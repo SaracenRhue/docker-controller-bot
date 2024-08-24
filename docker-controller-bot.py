9cef3852154508bae5be101e15f5d7ae4f571269
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2024-08-24 16:31:26 +0200

    u

--- docker-controller-bot.py
+++ docker-controller-bot.py
@@ -17,7 +17,7 @@ import json
 import requests
 import sys
 
-VERSION = "3.2.1"
+VERSION = "SR-0.1"
 
 def debug(message):
 	print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - DEBUG: {message}')
